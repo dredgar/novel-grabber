f394decb021bd54774c322a709636a2c5091d341
kyr1no <dredgar065@gmail.com>
2018-12-22 22:57:42 +0800

    >_<

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 from nvg import *
 
-for x in ['tieba', 'yamibo', 'nyasama']:
-    load_cookies(f'{x}-cookies.json', x)
-
-generate_collection([Nyasama(x) for x in [49451, 30279]], 'test.txt', 'number')
+Yamibo(158054).generate()
